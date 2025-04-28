c25d0376e49f6671b6f081c328967199d68a8605
Veltan <uig54289@contiwan.com>
2025-04-28 12:32:47 +0300

    made the command

---
+++ command.sh
@@ -0,0 +1,19 @@
+CONFIG_FILE="config.ini"
+REPORT_PREFIX="output"
+BENCHMARKS_DIR="./m2s-bench-parsec-3.0"
+BENCHMARKS=(
+  "blackscholes"
+  "bodytrack"
+  "canneal"
+  "dedup"
+  "facesim"
+  "ferret"
+  "fluidanimate"
+  "streamcluster"
+  "swaptions"
+  "vips"
+)
+for BENCHMARK in "${BENCHMARKS[@]}"; do
+  REPORT_FILE="${REPORT_PREFIX}_${BENCHMARK}.txt"
+  m2s --x86-sim-detailed --x86-config "$CONFIG_FILE" --x86-report "output/$REPORT_FILE" "$BENCHMARKS_DIR/$BENCHMARK/$BENCHMARK" 2
+done
